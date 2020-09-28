23214128f38701bb390ee8a40a5529ccc155b557
Akuiron <portfolio@akuiron.com>
2020-09-28 08:53:59 +0200

    Add comments in code

--- functions.php
+++ functions.php
@@ -1,10 +1,12 @@
 <?php
 
-function register_my_menu(){ // this function save menu
+// this function save menu
+function register_my_menu(){ 
     register_nav_menu('main', "Main menu");
  }
  add_action('after_setup_theme', 'register_my_menu');
 
+// Permet d'afficher le logo
  function nafa_custom_logo_setup() {
     $defaults = array(
     'height'      => 100,
